33f243535d9dd45747c7e7a2e0289c549ea95e44
Colin Ogoo <ogoo.colin@gmail.com>
2024-07-04 09:20:07 +0000

    chore(unused routes): remove unused routes

--- api/index.js
+++ api/index.js
@@ -2,8 +2,6 @@
 import { Hono } from 'hono';
 import wallet from '../src/routes/wallet/index.js';
 import transfer from '../src/routes/transfer/index.js';
-import convert from '../src/routes/convert/index.js';
-import request from '../src/routes/request/index.js';
 import intents from '../src/routes/intents/index.js';
 import apiKeys from '../src/routes/admin/index.js';
 import { handle } from '@hono/node-server/vercel';
@@ -18,8 +16,6 @@ const app = new Hono().basePath('/api');
 
 app.route('/v1/wallet', wallet);
 app.route('/v1/transfer', transfer);
-app.route('/v1/convert', convert);
-app.route('/v1/request', request);
 app.route('/v1/intents', intents);
 app.route('/v1/admin/api-keys', apiKeys);
 
