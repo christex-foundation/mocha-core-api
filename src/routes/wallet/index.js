fb65343ad511df6e7a6082b32d40a313defceb0f
Sparrow <benjaminthorpe19@gmail.com>
2025-03-27 11:26:07 +0000

    add endpoint to get the wallet address from phone number

--- src/routes/wallet/index.js
+++ src/routes/wallet/index.js
@@ -1,18 +1,25 @@
 //@ts-check
-import { Hono } from 'hono';
-import { fetchWalletBalance } from './wallet.js';
-import { apiKeyAuth } from '../../middleware/api-key-auth.js';
-import { errorHandler } from '../../middleware/error-handler.js';
+import { Hono } from "hono";
+import { fetchWalletBalance } from "./wallet.js";
+import { apiKeyAuth } from "../../middleware/api-key-auth.js";
+import { errorHandler } from "../../middleware/error-handler.js";
 
 const app = new Hono();
-app.use('/*', apiKeyAuth);
+app.use("/*", apiKeyAuth);
 app.onError(errorHandler);
 
-app.get('/:phone_number', async (c) => {
-  const phoneNumber = c.req.param('phone_number');
+app.get("/:phone_number", async (c) => {
+	const phoneNumber = c.req.param("phone_number").replace("+", "");
 
-  const balance = await fetchWalletBalance(phoneNumber);
-  return c.json({ balance });
+	const { balance } = await fetchWalletBalance(phoneNumber);
+	return c.json({ balance });
+});
+
+app.get("/:phone_number/address", async (c) => {
+	const phoneNumber = c.req.param("phone_number").replace("+", "");
+
+	const { address, balance } = await fetchWalletBalance(phoneNumber);
+	return c.json({ address, balance });
 });
 
 export default app;
