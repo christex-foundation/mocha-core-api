15777cabbca866aba09d31ef60e153bc0be0d982
Sparrow <benjaminthorpe19@gmail.com>
2025-06-13 16:24:20 +0000

    feat(refactor): return the account - balance and address back to user

--- src/routes/wallet/index.js
+++ src/routes/wallet/index.js
@@ -1,18 +1,18 @@
 //@ts-check
-import { Hono } from 'hono';
-import { fetchWalletBalance } from './wallet.js';
-import { apiKeyAuth } from '../../middleware/api-key-auth.js';
-import { errorHandler } from '../../middleware/error-handler.js';
+import { Hono } from "hono";
+import { apiKeyAuth } from "../../middleware/api-key-auth.js";
+import { errorHandler } from "../../middleware/error-handler.js";
+import { fetchWalletBalance } from "./wallet.js";
 
 const app = new Hono();
-app.use('/*', apiKeyAuth);
+app.use("/*", apiKeyAuth);
 app.onError(errorHandler);
 
-app.get('/:phone_number', async (c) => {
-  const phoneNumber = c.req.param('phone_number');
+app.get("/:phone_number", async (c) => {
+	const phoneNumber = c.req.param("phone_number");
 
-  const balance = await fetchWalletBalance(phoneNumber);
-  return c.json({ balance });
+	const account = await fetchWalletBalance(phoneNumber);
+	return c.json({ account });
 });
 
 export default app;
--- src/routes/wallet/wallet.js
+++ src/routes/wallet/wallet.js
@@ -1,26 +1,29 @@
 //@ts-check
 
-import * as walletRepository from '../../repos/wallets.js';
-import { createNotFoundError } from '../../utils/errors.js';
-import { deriveAddress, MOCHA_KEYPAIR } from '../../utils/solana.js';
+import * as walletRepository from "../../repos/wallets.js";
+import { createNotFoundError } from "../../utils/errors.js";
+import { MOCHA_KEYPAIR, deriveAddress } from "../../utils/solana.js";
 
 /**
  * @description Function to fetch the wallet balance
  * @param {string} phoneNumber
  */
 export async function fetchWalletBalance(phoneNumber) {
-  const addressPk = await deriveAddress(MOCHA_KEYPAIR.publicKey, phoneNumber);
+	const addressPk = await deriveAddress(MOCHA_KEYPAIR.publicKey, phoneNumber);
 
-  try {
-    const balance = await walletRepository.getTokenAccountBalance(addressPk);
-    return balance?.value?.uiAmount?.toFixed(2);
-  } catch (error) {
-    console.error('Error fething balance; Token account not found', {
-      error,
-      phoneNumber,
-      address: addressPk.toBase58(),
-    });
+	try {
+		const balance = await walletRepository.getTokenAccountBalance(addressPk);
+		return {
+			address: addressPk.toBase58(),
+			balance: balance?.value?.uiAmount?.toFixed(2),
+		};
+	} catch (error) {
+		console.error("Error fething balance; Token account not found", {
+			error,
+			phoneNumber,
+			address: addressPk.toBase58(),
+		});
 
-    return 0;
-  }
+		return 0;
+	}
 }
